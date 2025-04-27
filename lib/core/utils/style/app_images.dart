362c8cab57c1b8595ed342716ccf07fe08b4edc1
tarek_elmarsafy <tarekelmarsafy50@gmail.com>
2025-04-27 03:55:59 +0300

    add images as svg and create app_images.dart

---
+++ lib/core/utils/style/app_images.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for kImagesAdd
+  /// asstes/images/add.svg
+  static const String kImagesAdd = "asstes/images/add.svg";
+
+  /// Assets for kImagesArrowDown
+  /// asstes/images/arrow-down.svg
+  static const String kImagesArrowDown = "asstes/images/arrow-down.svg";
+
+  /// Assets for kImagesAvatar1
+  /// asstes/images/avatar1.svg
+  static const String kImagesAvatar1 = "asstes/images/avatar1.svg";
+
+  /// Assets for kImagesAvtar2
+  /// asstes/images/avtar2.svg
+  static const String kImagesAvtar2 = "asstes/images/avtar2.svg";
+
+  /// Assets for kImagesBalance
+  /// asstes/images/balance.svg
+  static const String kImagesBalance = "asstes/images/balance.svg";
+
+  /// Assets for kImagesCardPymint
+  /// asstes/images/card_pymint.svg
+  static const String kImagesCardPymint = "asstes/images/card_pymint.svg";
+
+  /// Assets for kImagesCardSend
+  /// asstes/images/card-send.svg
+  static const String kImagesCardSend = "asstes/images/card-send.svg";
+
+  /// Assets for kImagesDashboard
+  /// asstes/images/dashboard.svg
+  static const String kImagesDashboard = "asstes/images/dashboard.svg";
+
+  /// Assets for kImagesEllipse 848
+  /// asstes/images/Ellipse 848.svg
+  static const String kImagesEllipse = "asstes/images/Ellipse 848.svg";
+
+  /// Assets for kImagesGallery
+  /// asstes/images/gallery.svg
+  static const String kImagesGallery = "asstes/images/gallery.svg";
+
+  /// Assets for kImagesLogout
+  /// asstes/images/logout.svg
+  static const String kImagesLogout = "asstes/images/logout.svg";
+
+  /// Assets for kImagesMoneys
+  /// asstes/images/moneys.svg
+  static const String kImagesMoneys = "asstes/images/moneys.svg";
+
+  /// Assets for kImagesMyInvestments
+  /// asstes/images/my_investments.svg
+  static const String kImagesMyInvestments = "asstes/images/my_investments.svg";
+
+  /// Assets for kImagesMyTransaction
+  /// asstes/images/my_transaction.svg
+  static const String kImagesMyTransaction = "asstes/images/my_transaction.svg";
+
+  /// Assets for kImagesSetting
+  /// asstes/images/setting.svg
+  static const String kImagesSetting = "asstes/images/setting.svg";
+
+  /// Assets for kImagesStatistics
+  /// asstes/images/statistics.svg
+  static const String kImagesStatistics = "asstes/images/statistics.svg";
+
+  /// Assets for kImagesWallet2
+  /// asstes/images/wallet-2.svg
+  static const String kImagesWallet2 = "asstes/images/wallet-2.svg";
+}
---
+++ lib/core/utils/style/app_style.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppStyle {
+  static const TextStyle styleRegular16 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle styleBold16 = TextStyle(
+    color: Color(0xff4eb7f2),
+    fontSize: 16,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle styleMedium16 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleMedium20 = TextStyle(
+    color: Color(0xffffffff),
+    fontSize: 16,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleSemiBold16 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleSemiBold20 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 20,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular12 = TextStyle(
+    color: Color(0xffaaaaaa),
+    fontSize: 12,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemiBold24 = TextStyle(
+    color: Color(0xff4eb7f2),
+    fontSize: 24,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular14 = TextStyle(
+    color: Color(0xffaaaaaa),
+    fontSize: 14,
+    fontFamily: 'Montsrrat',
+    fontWeight: FontWeight.w400,
+  );
+}
